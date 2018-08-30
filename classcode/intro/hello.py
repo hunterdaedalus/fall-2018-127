88f4550327dfb73532d5edd5646f6c0935cd4448
Mike Zamansky <zamansky@gmail.com>
2018-08-30 09:27:47 -0400

    added hello.py

---
+++ classcode/intro/hello.py
@@ -0,0 +1,16 @@
+print("STARTING")
+a=5+2+100 
+b = a + 10
+print("a = ",a)
+print("b = ",b)
+a=a-100
+print("a = ",a)
+print("b = ",b)
+
+print(10/3)
+
+s1 = "Hello"
+s2 = "world"
+print(s1 + " " + s2)
+print(5**3)
+print("DONE")

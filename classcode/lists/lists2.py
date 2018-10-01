9cfabcf56ac814c038d1b5f6494f6977c13030ef
Mike Zamansky <zamansky@gmail.com>
2018-10-01 09:22:42 -0400

    fiunished build_random_list

--- classcode/lists/lists2.py
+++ classcode/lists/lists2.py
@@ -1,5 +1,23 @@
 
 import random
 
-random.randrange(5,10)
+def build_random_list(items, max_value):
+    """
+    create and return a list filled with items number of element
+    each element should be a random integer value
+    between 0 and 99
+    """
+    l=[]
+    # your code here
+    i = 0
+    while i < items:
+        #l.append(random.randrange(0,max_value))
+        l = l + [ random.randrange(0,max_value) ]
+        i = i + 1
+        
+    return l
 
+l = build_random_list(15,100)
+print(l)
+l2 = build_random_list(8,1000)
+print(l2)

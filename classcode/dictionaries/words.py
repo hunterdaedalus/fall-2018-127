e43d56deab018a34046036cdd0e3668e0fef7bca
Mike Zamansky <zamansky@gmail.com>
2018-11-08 08:56:36 -0500

    added macbeth.txt and words.py

--- classcode/dictionaries/words.py
+++ classcode/dictionaries/words.py
@@ -1,8 +1,14 @@
 
 
-def build_word_counts():
-    pass
+def build_word_counts(words):
+    d={}
+    for word in words.split():
+        d.setdefault(word,0)
+        d[word]=d[word]+1
+    return d
 
 
 
 s = "one two three four five four six three seven three two three eight nine"
+d=build_word_counts(s)
+print(d)

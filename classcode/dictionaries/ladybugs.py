f6ae516d05e99b3f4a26b9555151fe5f68e11817
Mike Zamansky <zamansky@gmail.com>
2018-11-08 08:43:01 -0500

    added ladybugs

--- classcode/dictionaries/ladybugs.py
+++ classcode/dictionaries/ladybugs.py
@@ -1,3 +1,21 @@
+def happy2(s):
+    if '_' not in s:
+        for i in range(1,len(s)-1):
+            if s[i] != s[i-1] and s[i] != s[i+1]:
+                return False
+        if s[0] != s[1] or s[len(s)-1] != s[len(s)-2]:
+            return False
+        return True
+    else:
+        s = s.replace("_","")
+        s = sorted(s)
+        for i in range(1,len(s)-1):
+            if s[i] != s[i-1] and s[i] != s[i+1]:
+                return False
+        if s[0] != s[1] or s[len(s)-1] != s[len(s)-2]:
+            return False
+        return True
+
 
 def happy(s):
     if '_' not in s:
@@ -27,5 +45,6 @@ testcases = ["abcdea","abccde","aabbcccdd","abc_def","abc_ecdccdeba__","abc_abcd
 for test in testcases:
     print(test)
     print(happy(test))
-    
+    print(happy2(test))
+    print("------------")
 

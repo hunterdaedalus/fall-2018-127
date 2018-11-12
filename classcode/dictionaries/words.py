7243a2d3d68ce7beca1899a3ae056fedb1ff6f98
Mike Zamansky <zamansky@gmail.com>
2018-11-12 08:56:00 -0500

    intro'd list comprehensions

--- classcode/dictionaries/words.py
+++ classcode/dictionaries/words.py
@@ -16,12 +16,21 @@ def clean_data(s):
             result = result + " "
     return result
 
-filename="/home/zamansky/gh/fall-2018-127/classcode/dictionaries/macbeth.txt"
+filename="/home/zamansky/gh/fall-2018-127/classcode/dictionaries/moby-small.txt"
+
 f = open(filename)
 s = f.read()
-words_uncleaned = build_word_counts(s)
-print(words_uncleaned)
+#words_uncleaned = build_word_counts(s)
+#print(words_uncleaned)
+#print("-------------------")
 cleaned_string = clean_data(s)
-print("-------------------")
 words = build_word_counts(cleaned_string)
-print(words)
+vals = words.values()
+vals = sorted(vals)
+#print('------')
+#common_words = []
+#for k in words:
+#    if words[k] > 1000:
+#        common_words.append([k,words[k]])
+
+common_words = [ [k,words[k]] for k in words if words[k] > 50 ]

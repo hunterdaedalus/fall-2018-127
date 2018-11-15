ad278e6a7b6050d466c06a3035d4c2fca3aab973
Mike Zamansky <zamansky@gmail.com>
2018-11-15 09:23:01 -0500

    finished markov chain text generator

--- classcode/dictionaries/words.py
+++ classcode/dictionaries/words.py
@@ -27,21 +27,37 @@ def build_word_lists_by_index(words):
 def build_word_lists(words):
     d={}
     words = words.split()
-    prev = words[0]
-    for nextword in words[1:]:
-        d.setdefault(prev,[])
-        d[prev].append(nextword)
-        prev = nextword
+    first = words[0]
+    second = words[1]
+    for nextword in words[2:]:
+        key = (first,second)
+        d.setdefault(key,[])
+        d[key].append(nextword)
+        first = second
+        second = nextword
     return d
 
-filename="/home/zamansky/gh/fall-2018-127/classcode/dictionaries/moby-small.txt"
+import random
+def gen_text(wl,number,tuple):
+    first  = tuple[0]
+    second = tuple[1]
+    text=[]
+    for i in range(number):
+        text.append(first)
+        next = random.choice(wl[tuple])
+        tuple = (second,next)
+        first = second
+        second = next
+    return ' '.join(text)
 
-f = open(filename)
-s = f.read()
+#filename="/home/zamansky/gh/fall-2018-127/classcode/dictionaries/moby-small.txt"
+
+#f = open(filename)
+#s = f.read()
 #words_uncleaned = build_word_counts(s)
 #print(words_uncleaned)
 #print("-------------------")
-cleaned_string = clean_data(s)
+#cleaned_string = clean_data(s)
 #words = build_word_counts(cleaned_string)
 #vals = words.values()
 #vals = sorted(vals)
@@ -50,19 +66,24 @@ cleaned_string = clean_data(s)
 
 
 
-filename="/home/zamansky/gh/fall-2018-127/classcode/dictionaries/moby-small.txt"
+filename="/home/zamansky/gh/fall-2018-127/classcode/dictionaries/psalms.txt"
 
 f = open(filename)
 s = f.read()
 s = clean_data(s)
+slist = s.split()
 wl = build_word_lists(s)
+print(gen_text(wl,100,(slist[0],slist[1])))
+
+#story = gen_text(wl,100,s.split()[0])
+#print(story)
 #words_uncleaned = build_word_counts(s)
 #print(words_uncleaned)
 #print("-------------------")
-cleaned_string = clean_data(s)
-words = build_word_counts(cleaned_string)
-vals = words.values()
-vals = sorted(vals)
+#cleaned_string = clean_data(s)
+#words = build_word_counts(cleaned_string)
+#vals = words.values()
+#vals = sorted(vals)
 #print('------')
 #common_words = []
 #for k in words:

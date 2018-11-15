534f1de9a16f69da6df81cfa02a5f2bdbf9aa062
Mike Zamansky <zamansky@gmail.com>
2018-11-15 08:55:23 -0500

    added word_list code

--- classcode/dictionaries/words.py
+++ classcode/dictionaries/words.py
@@ -16,10 +16,46 @@ def clean_data(s):
             result = result + " "
     return result
 
+def build_word_lists_by_index(words):
+    d={}
+    words = words.split()
+    for i in range(0,len(words)-2):
+        d.setdefault(words[i],[])
+        d[words[i]].append(words[i+1])
+    return d
+
+def build_word_lists(words):
+    d={}
+    words = words.split()
+    prev = words[0]
+    for nextword in words[1:]:
+        d.setdefault(prev,[])
+        d[prev].append(nextword)
+        prev = nextword
+    return d
+
 filename="/home/zamansky/gh/fall-2018-127/classcode/dictionaries/moby-small.txt"
 
 f = open(filename)
 s = f.read()
+#words_uncleaned = build_word_counts(s)
+#print(words_uncleaned)
+#print("-------------------")
+cleaned_string = clean_data(s)
+#words = build_word_counts(cleaned_string)
+#vals = words.values()
+#vals = sorted(vals)
+#print('------')
+#common_words = []
+
+
+
+filename="/home/zamansky/gh/fall-2018-127/classcode/dictionaries/moby-small.txt"
+
+f = open(filename)
+s = f.read()
+s = clean_data(s)
+wl = build_word_lists(s)
 #words_uncleaned = build_word_counts(s)
 #print(words_uncleaned)
 #print("-------------------")
@@ -33,4 +69,11 @@ vals = sorted(vals)
 #    if words[k] > 1000:
 #        common_words.append([k,words[k]])
 
-common_words = [ [k,words[k]] for k in words if words[k] > 50 ]
+#common_words = [ [k,words[k]] for k in words if words[
+#for k in words:
+#    if words[k] > 1000:
+#        common_words.append([k,words[k]])
+
+#common_words = [ [k,words[k]] for k in words if words[k] > 50 ]
+
+    
